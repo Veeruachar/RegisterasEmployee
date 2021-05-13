62e4555fc17de75b8843f04261f7f43087c02381
Veeruachar <78130880+Veeruachar@users.noreply.github.com>
2021-05-13 18:54:10 +0530

    springbootinitializer added

--- src/main/java/com/Asos/Demo/Empregistration1Application.java
+++ src/main/java/com/Asos/Demo/Empregistration1Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Empregistration1Application {
+public class Empregistration1Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Empregistration1Application.class, args);
